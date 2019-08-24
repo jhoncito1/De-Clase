ea85438032858b97db53ba11513da9eae4557bf9
Jhon Chavez <jjchavez44@misena.edu.co>
2019-08-24 16:39:48 -0500

    tdy

--- clasephp/index.php
+++ clasephp/index.php
@@ -1,3 +1,15 @@
+<?php
+function print_input($datos)
+{
+    if ($_POST[$datos]) {
+        echo htmlspecialchars($_POST($datos), ENT_QUOTES);
+    }
+}
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,40 +25,9 @@
     <header></header>
     <nav></nav>
     <aside>
-        <p>aside Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsa impedit voluptas, dignissimos eveniet sunt ex saepe consequatur placeat amet ducimus error molestias sit voluptate omnis quas mollitia similique excepturi?</p>
+        <p>asise</p>
     </aside>
     <main>
-
-        <form action="index.php" method="POST">
-            <div>
-                <label>numero inicial</label>
-                <input type="number" name="num1"><br><br>
-                <label>numero final </label>
-                <input type="number" name="num2"><br><br>
-                <label>multiplo</label>
-                <input type="number" name="num3"><br>
-            </div>
-
-            <input type="submit" name="boton" value="boton2">
-        </form>
-
-        <?php
-        if (isset($_POST['boton'])) {
-            $num1 = $_POST['num1'];
-            $num2 = $_POST['num2'];
-            $num3 = $_POST['num3'];
-
-
-            echo "los multiplos de " . $num3 . " hasta " . $num2 .  " son: <br>";
-
-            for ($i = $num1; $i <= $num2; $i++) {
-                if ($i % $num3 == 0) {
-                    $num[] = $i;
-                }
-            }
-            print_r($num);
-        }
-        ?>
 
         <form method="POST" action="index.php">
 
@@ -68,15 +49,20 @@
         </form>
 
         <?php
-        if (isset($_POST['enviar'])) {
-            if (empty($_POST['n2']) or empty($_POST['operacion'])) {
+        if (isset($_POST['enviar'])) 
+        {
+            if (empty($_POST['n2']) or empty($_POST['operacion'])) 
+            {
                 echo "todas las casillas deben tener valor y elegir operacion";
-            } else {
+            } 
+            else 
+            {
                 $num1 = $_POST['n1'];
                 $num2 = $_POST['n2'];
 
 
-                switch ($_POST['operacion']) {
+                switch ($_POST['operacion']) 
+                {
                     case 1:
                         $result = $num1 + $num2;
                         echo "<br> El resultado de " . $num1 . " + " . $num2 . " = " . $result;
@@ -90,15 +76,15 @@
                         echo "<br> El resultado de " . $num1 . " * " . $num2 . " = " . $result;
                         break;
                     case 4:
-                        $result = number_format(($num1 / $num2), 2, ",", ".");
+                        $result = number_format(( $num1 / $num2),2,",",".");
                         echo "<br> El resultado de " . $num1 . " / " . $num2 . " = " . $result;
                         break;
                     case 5:
-                        echo "<br> el resultado es " . pow($num1, 1 / $num2);
+                        echo "<br> el resultado es " .pow($num1,1/$num2);
                         break;
                     case 6:
-                        echo "<br> el resultado es " . pow($num1, $num2);
-                        break;
+                        echo "<br> el resultado es " .pow($num1, $num2);
+                    break;  
                         break;
 
                     default:
@@ -115,7 +101,7 @@
 
 
     </main>
-    <footer> </footer>
+    <footer></footer>
 </body>
 
 </html>
---
+++ clasephp/multiplos.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/multiplos.css">
+</head>
+
+<body>
+    <header></header>
+    <nav></nav>
+    <aside>
+        <p>p</p>
+    </aside>
+    <main>
+        <form action="index.php" method="POST">
+            <div>
+                <label>num 1</label>
+                <input type="number" name="num1">
+                <label>num 2</label>
+                <input type="number" name="num2">
+            </div>
+            <button type="submit">boton1</button>
+            <input type="submit" name="boton" value="boton2">
+        </form>
+        <?php
+        if (isset($_POST['boton'])) {
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
+            echo ("la suma de " . $num1 . " + " . $num2 . " es: " . ($num2 + $num1));
+        }
+        $num = array('5' => 2, 5, 7, "hola");
+        print_r($num);
+
+        for ($i = 0; $i < 11; $i++) {
+            $num2[$i] = $i;
+        }
+        echo "<br>";
+        print_r($num2);
+        echo "posision 8 = " . $num[8];
+
+        ?>
+
+    </main>
+    <footer></footer>
+</body>
+
+</html>
+
+
+
+<form action="index.php" method="POST">
+    <div>
+        <label>Tabla del</label>
+        <input type="number" name="num1">
+        <label>Desde </label>
+        <input type="number" name="num2">
+        <label>Hasta</label>
+        <input type="number" name="num3">
+    </div>
+
+    <input type="submit" name="boton" value="enviar">
+</form>
+
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$num3 = $_POST['num3'];
+
+
+echo "l" . $num3 . "  " . $num2 .  " ";
+
+for ($i = $num2; $i <= $num3; $i++) {
+    if ($i % $num1 == 0) {
+        $num[] = $i;
+    }
+}
+print_r($num);
+?>
