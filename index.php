7dd8a6d950d4b83fddb4597bbf2bfe4f2adeee4a
Jhon Chavez <47484254+jhoncito1@users.noreply.github.com>
2019-09-10 22:57:24 -0500

    k

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sitio css</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="contenedor">
+        <header class="header">
+            <h2>Header</h2>
+        </header>
+        <main class="contenido">
+            <p>
+                .....
+            </p>
+        </main>
+        <aside class="sidebar">
+            <h3>sidebar</h3>
+        </aside>
+        <div class="widget-1">
+            <h3>witget 1</h3>
+        </div>
+        <div class="witget-2">
+            <h3>witget 2</h3>
+        </div>
+        <footer class="footer">
+            <h3>pie de pagina</h3>
+        </footer>
+    </div>
+</body>
+</html>
